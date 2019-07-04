7795eb99c728df54edc3cd981d1a88592931016d
Michael Garas <mgaras@Michaels-MacBook-Pro-2.local>
2019-07-04 16:34:42 -0400

    Added article endpoints and Views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
     
     
-def index
-end
+    def index
+    end
 
-def about
-end
+    def about
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get 'about', to: 'pages#about'
-
+  resources :articles
+  
 end
