f0ae29607ccdececbf2554397652c1cf0c362c4d
Michael Garas <michael.luka@hotmail.com>
2019-07-05 01:24:48 -0400

    Created users table and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
 
   get 'about', to: 'pages#about'
   resources :articles
-  
 end
