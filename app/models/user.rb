08d19b7986038a60d64a6041250d3c7967140937
Michael Garas <michael.luka@hotmail.com>
2019-07-05 01:48:23 -0400

    Added association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   before_save { self.email = email.downcase }
+
+  has_many :articles
 end
