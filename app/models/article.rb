bd544a073693cb025c2a0801d806d4beef3a318b
Michael Garas <michael.luka@hotmail.com>
2019-07-10 22:40:29 -0400

    Added the character limit for description

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 3000 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 4000 }
 
   belongs_to :user
   validates :user_id, presence: true
